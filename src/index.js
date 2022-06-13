3c6e0b71c5459861ccf0686b5ba49887b99d024a
tgoandrex <tgoandrex@gmail.com>
2022-06-13 17:25:02 -0500

    Change package.json

--- src/index.js
+++ src/index.js
@@ -1,14 +1,4 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
 import { run } from "../app";
 import "./main.css";
-
-express()
-    .use(express.static(path.join(__dirname, 'public')))
-    .set('views', path.join(__dirname, 'views'))
-    .set('view engine', 'ejs')
-    .get('/', (req, res) => res.render('pages/index'))
-    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 run();
